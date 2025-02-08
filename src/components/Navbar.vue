6b49451242ae66277cb754f4fc36b6b92480f84b
LEQUACHTIENB2014886 <lequachtien123@gmail.com>
2025-02-08 10:12:42 +0700

    images

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,51 @@
 <template>
-    navbar
-  </template>
-  
-  <script setup>
-  
-  </script>
-  
-  <style>
-  
-  </style>
+  <nav class="navbar">
+    <ul>
+      <li><a href="#">Input</a></li>
+      <li><a href="#">Analyze</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  background-color: green;
+  padding: 15px;
+  display: flex;
+  justify-content: center;
+}
+
+.navbar ul {
+  list-style: none;
+  display: flex;
+  gap: 20px;
+  padding: 0;
+  margin: 0;
+}
+
+.navbar li {
+  display: inline;
+}
+
+.navbar a {
+  color: white;
+  text-decoration: none;
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.navbar a:hover {
+  text-decoration: underline;
+}
+
+@media (max-width: 768px) {
+  .navbar {
+    flex-direction: column;
+    align-items: center;
+  }
+  .navbar ul {
+    flex-direction: column;
+    gap: 10px;
+  }
+}
+</style>
